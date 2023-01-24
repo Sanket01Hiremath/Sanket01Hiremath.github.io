e97cd6a7865d2edf543b54a5a525d0fc3f0f4824
Sanket Hiremath <73591040+Sanket01Hiremath@users.noreply.github.com>
2023-01-24 22:06:02 +0530

    Updated

---
+++ Script/Script.js
@@ -0,0 +1,44 @@
+let image=document.getElementById("image");
+    let about=document.getElementById("about");
+    let div1=document.querySelector("#projects>div>div:first-child");
+    let div2=document.querySelector("#projects>div>div:last-child");
+    let div3=document.querySelector("#projects>div:last-child>div:first-child");
+    let div4=document.querySelector("#projects>div:last-child>div:last-child");
+    let home=document.getElementById("home");
+    let git=document.getElementById("git");
+
+    window.onload=(e)=>{
+        image.style.width="80%";
+        image.style.opacity="1";
+        about.style.opacity="1";
+        about.style.transition="2s";
+        home.style.top="0px";
+        home.style.opacity="1";
+        home.style.transition="2s";
+        git.style.opacity="1";
+        git.style.transition="2s";
+    }
+    let slide=document.getElementById("slider");
+    let closer=document.getElementById("close");
+    let menu=document.getElementById("slidermenu");
+    let html=document.querySelector("html");
+
+    slide.addEventListener("click",(e)=>{
+            menu.style.height="100%";
+            menu.style.transition="1s";
+            menu.style.opacity="1";
+            html.style.overflowY="hidden";
+            menu.style.top="0";
+    })
+    closer.addEventListener("click",(e)=>{
+            menu.style.height="0";
+            menu.style.transition="1s";
+            menu.style.opacity="0";
+            html.style.overflowY="scroll";
+            menu.style.top="45px";
+    })
+
+    let feed=document.getElementById("btn");
+    feed.addEventListener("click",(e)=>{
+        alert("ThankYou for your valuable feedback!");
+    })
